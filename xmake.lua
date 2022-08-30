037cd0a1f69d5152372d1eee7027c8e24aea438f
Romain Chardiny <romain.chardiny@gmail.com>
2022-08-30 11:13:12 +0200

    improve xmake file

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-add_rules("mode.debug", "mode.release", "mode.asan", "mode.valgrind", "mode.lsan")
+add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
 
@@ -9,7 +9,7 @@ target("calculator")
     add_headerfiles("include/(calculator/**.hpp)")
     add_includedirs("include", { public = true })
     add_files("src/calculator/**.cpp")
-    add_packages("fast_float", "fmt")
+    add_packages("fast_float")
 
     on_load(function (target)
         if target:kind() == "static" then
@@ -21,73 +21,3 @@ target("repl")
     set_kind("binary")
     add_deps("calculator")
     add_files("src/repl/*.cpp")
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro defination
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
-
