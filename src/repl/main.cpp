b05ebaceebc8ffd5729cbd022d791dfd63749f52
Romain Chardiny <romain.chardiny@gmail.com>
2022-08-31 16:09:10 +0200

    fix repl

--- src/repl/main.cpp
+++ src/repl/main.cpp
@@ -10,7 +10,7 @@ int main() {
   prefix();
   for (std::string line; std::getline(std::cin, line);) {
     try {
-      c.Execute(line + "\n");
+      c.Execute(line);
     } catch (std::exception& e) {
       std::cout << e.what() << std::endl;
     }
