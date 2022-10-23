1992dbdc5641831dc087ff0eb4c56dfdcc89b455
Romain Chardiny <romain.chardiny@gmail.com>
2022-10-23 19:14:03 +0200

    cleanup

--- xmake.lua
+++ xmake.lua
@@ -7,6 +7,29 @@ set_languages("cxx20")
 set_rundir("./build/$(plat)_$(arch)_$(mode)")
 set_targetdir("./build/$(plat)_$(arch)_$(mode)")
 
+add_requires("fast_float", "fmt")
+
+add_headerfiles("examples/*.calc")
+
+target("calculator")
+    set_kind("$(kind)")
+    set_group("Librairies")
+    
+    add_defines("CALCULATOR_BUILD")
+    
+    add_headerfiles("include/(calculator/**.hpp)")
+    add_includedirs("include", { public = true })
+    add_files("src/calculator/**.cpp")
+    
+    add_packages("fast_float", "fmt")
+
+    on_load(function (target)
+        if target:kind() == "static" then
+            target:add("defines", "CALCULATOR_STATIC", { public = true })
+        end
+    end)
+
+
 includes("tests/xmake.lua")
 includes("src/**.lua")
 includes("xmake/**.lua")
